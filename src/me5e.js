ace51eeff374019064fcdea4cbe413a0c996176b
RyanTreat <ryandtreat@gmail.com>
2025-10-09 23:45:53 -0700

    upload

---
+++ src/me5e.js
@@ -0,0 +1,249 @@
+//Changing out deprecated 5e skills to their replacements
+dnd5e.config.skills.arc = {label: "Electronics" , ability: "int"};
+dnd5e.config.skills.nat = {label: "Engineering" , ability: "int"};
+dnd5e.config.skills.rel = {label: "Science" , ability: "int"};
+dnd5e.config.skills.veh = {label: "Vehicle Handling" , ability: "dex"};
+// Deletes animal handling from system
+delete dnd5e.config.skills.ani;
+
+//Adding equipment types
+dnd5e.config.equipmentTypes["prog"] = "Program";
+dnd5e.config.equipmentTypes["armMod"] = "Armor Mod";
+dnd5e.config.equipmentTypes["wepMod"] = "Weapon Mod"
+dnd5e.config.equipmentTypes["bodArm"] = "Body Armor"
+
+
+// //Changing and adding consumable types
+dnd5e.config.consumableTypes["wand"] = "Single-Use Program"
+dnd5e.config.consumableTypes["rod"] = "Grenade";
+dnd5e.config.consumableTypes["narc"] = "Narcotic";
+
+const prep = dnd5e.documents.Actor5e.prototype.prepareBaseData
+function extendActorData() {
+	let myActorList = game.actors.filter(a => (a.type === 'character' || a.type === 'npc') === true);
+	const dat = this.system.skills
+	const keys =Object.keys(myActorList)
+	keys.forEach((key, index) => {myActorList[key].system.skills.veh.ability = "dex"});
+
+	const health = this.system.attributes.hp;
+	
+	if (this.type === "npc" || this.type === "character") {
+		health["shields"] = health["shields"] || 0;
+		health["shieldsMax"] = health["shieldsMax"] || 0;
+		health["shieldsRegen"] = health["shieldsRegen"] || 0;
+	}
+
+
+    return prep.call(this);
+}
+
+//Changing "schools" of magic
+dnd5e.config.spellSchools['abj'] = 'Biotics';
+dnd5e.config.spellSchools['con'] = 'Tech';
+dnd5e.config.spellSchools['div'] = 'Combat Powers';
+
+//Adding weapon types
+dnd5e.config.weaponTypes['ars'] = 'Assault Rifle';
+dnd5e.config.weaponTypes['hps'] = 'Heavy Pistol';
+dnd5e.config.weaponTypes['smg'] = 'SMG';
+dnd5e.config.weaponTypes['sht'] = 'Shotgun';
+dnd5e.config.weaponTypes['snp'] = 'Sniper Rifle';
+dnd5e.config.weaponTypes['hvy'] = 'Heavy Weapon';
+
+//Remove Weapon Types
+delete dnd5e.config.weaponTypes.WeaponSimpleM;
+
+//Adding weapon properties
+//delete dnd5e.config.itemProperties.ada;
+//dnd5e.Item.Property.Arc = "Arc";
+dnd5e.config.itemProperties.arc = {label: "Arc", isPhysical = true};
+dnd5e.config.validProperties.weapon.add("arc");
+//dnd5e.validProperties.weapon: new Set([
+  //  "arc"])
+//dnd5e.config.itemProperties['bst'] = 'Burst Fire';
+//dnd5e.config.itemProperties['dtp'] = 'Double Tap';
+//dnd5e.config.itemProperties['het'] = 'Heat';
+//dnd5e.config.itemProperties['hip'] = 'Hip Fire';
+//dnd5e.config.itemProperties['snt'] = 'Silent';
+//dnd5e.config.itemProperties['coi'] = 'Recoil';
+
+//Adding weapon proficiencies
+dnd5e.config.weaponProficiencies['ars'] = 'Assault Rifle';
+dnd5e.config.weaponProficiencies['hps'] = 'Heavy Pistol';
+dnd5e.config.weaponProficiencies['smg'] = 'SMG';
+dnd5e.config.weaponProficiencies['sht'] = 'Shotgun';
+dnd5e.config.weaponProficiencies['snp'] = 'Sniper Rifle';
+dnd5e.config.weaponProficiencies['hvy'] = 'Heavy Weapon';
+
+//Adding condition types
+dnd5e.config.conditionTypes['frozen'] = 'Frozen';
+dnd5e.config.conditionTypes['indoctrinated'] = 'Indoctrinated';
+dnd5e.config.conditionTypes['primed'] = 'Primed';
+dnd5e.config.conditionTypes['primedCold'] = 'Primed: Cold';
+dnd5e.config.conditionTypes['primedFire'] = 'Primed: Fire';
+dnd5e.config.conditionTypes['primedForce'] = 'Primed: Force';
+dnd5e.config.conditionTypes['primedLight'] = 'Primed: Lightning';
+dnd5e.config.conditionTypes['primedNec'] = 'Primed: Necrotic';
+dnd5e.config.conditionTypes['targeting'] = 'Targeting';
+dnd5e.config.conditionTypes['lifted'] = 'Lifted';
+
+
+
+//Changing and adding some tool proficiencies
+dnd5e.config.toolProficiencies['herb'] = "Chemist's Supplies";
+dnd5e.config.toolProficiencies['navg'] = 'Starship System (Navigation)';
+dnd5e.config.toolProficiencies['pois'] = "Brewer's Supplies";
+dnd5e.config.toolProficiencies['aswb'] = "Armorsmith's Workbench";
+dnd5e.config.toolProficiencies['h4ck'] = 'Hacking Tools';
+dnd5e.config.toolProficiencies['mdcn'] = 'Medical Kit';
+dnd5e.config.toolProficiencies['pntr'] = "Painter's Supplies";
+dnd5e.config.toolProficiencies['ssdr'] = 'Starship Systems (Drive)';
+dnd5e.config.toolProficiencies['sshe'] = 'Starship Systems (Helm)';
+dnd5e.config.toolProficiencies['sssc'] = 'Starship Systems (SSC)';
+dnd5e.config.toolProficiencies['ssew'] = 'Starship Systems (EWS)';
+dnd5e.config.toolProficiencies['sswp'] = 'Starship Systems (Weapons)';
+dnd5e.config.toolProficiencies['tail'] = "Tailor's Tools";
+dnd5e.config.toolProficiencies['tink'] = "Tinker's Tools";
+dnd5e.config.toolProficiencies['wswb'] = "Weaponsmith's Workbench";
+
+//Character sheets
+class ME5eCharacterSheet extends dnd5e.applications.actor.ActorSheet5eCharacter {
+    static get defaultOptions() {
+        console.log('~~~~~~~~~~~ME5E CHARACTER SHEET ACTIVE~~~~~~~~~~~');
+        const options = super.defaultOptions;
+        options.classes.push('me5e');
+        return options;
+    }
+}
+
+class ME5eParagonCharacterSheet extends dnd5e.applications.actor.ActorSheet5eCharacter {
+    static get defaultOptions() {
+        console.log('~~~~~~~~~~~PARAGON CHARACTER SHEET ACTIVE~~~~~~~~~~~');
+        const options = super.defaultOptions;
+        options.classes.push('paragon');
+        return options;
+    }
+}
+
+class ME5eRenegadeCharacterSheet extends dnd5e.applications.actor.ActorSheet5eCharacter {
+    static get defaultOptions() {
+        console.log('~~~~~~~~~~~RENEGADE CHARACTER SHEET ACTIVE~~~~~~~~~~~');
+        const options = super.defaultOptions;
+        options.classes.push('renegade');
+        return options;
+    }
+}
+
+console.log(`Registering character sheets for ME5e Module`);
+
+Actors.registerSheet('DND5E', ME5eCharacterSheet, {
+    types: ['character'],
+    makeDefault: true,
+});
+
+Actors.registerSheet('DND5E', ME5eParagonCharacterSheet, {
+    types: ['character'],
+    makeDefault: false,
+});
+
+Actors.registerSheet('DND5E', ME5eRenegadeCharacterSheet, {
+    types: ['character'],
+    makeDefault: false,
+});
+//Other sheets
+class ME5eNPCSheet extends dnd5e.applications.actor.ActorSheet5eNPC {
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.classes.push('me5e');
+        return options;
+    }
+}
+Actors.registerSheet('DND5E', ME5eNPCSheet, {
+    types: ['npc'],
+    makeDefault: true,
+});
+class ME5eItemSheet extends dnd5e.applications.item.ItemSheet5e {
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.classes.push('me5e');
+        return options;
+    }
+}
+Items.registerSheet('DND5E', ME5eItemSheet, {
+    types: [
+        'spell',
+        'weapon',
+        'equipment',
+        'loot',
+        'tool',
+        'backpack',
+        'consumable',
+        'class',
+        'feat',
+    ],
+    makeDefault: true,
+});
+
+//Adding a field to the header for shield tracker
+Hooks.on('renderActorSheet', (app, html, data) => {
+    const healthdiv = html
+        .find('header.sheet-header')
+        .find('ul.attributes.flexrow');
+    const flags = data.actor.flags.me5e || {};
+    healthdiv.prepend(`
+		  		<li class="attribute shields">
+                    <h4 class="attribute-name box-title">Shields</h4>
+                    <div class="attribute-value multiple">
+                        <input name="flags.me5e.shields" type="text" value="${flags.shields ?? 0}" data-dtype="Number" placeholder="5"/>
+                        <span class="sep"> / </span>
+                        <input name="flags.me5e.shieldsMax" type="text" value="${flags.shieldsMax ?? 0}" data-dtype="Number" placeholder="5"/>
+                    </div>
+                    <footer class="attribute-footer">
+                        <input name="flags.me5e.shieldsRegen" type="text" class="shieldsRegen" placeholder="Shield Regen." value="${flags.shieldsRegen ?? 0}" data-dtype="Number"/>
+                    </footer>
+                </li>
+	  `);
+
+
+    const col = html
+        .find('section.center-pane.flexcol')
+        .find('ul.attributes.flexrow');
+    const counters = html.find('div.counters');
+    counters.append(`
+	<div class="counter flexrow indoctrination">
+    <h4> Indoctrination </h4>
+    <div class="counter-value">
+      <input type="text" name="flags.me5e.indoctrination" placeholder="0" value="${
+          flags.indoctrination ?? 0
+      }" data-dtype="Number"/>
+    </div>
+	</div>
+	
+	`);
+    col.prepend(`
+	
+    <li class="attribute reputation">
+        <h4 class="attribute-name box-title">Reputation</h4>
+        <div class="attribute-value attributable">
+        <input type="text" name="flags.me5e.paragon" placeholder="0" value="${
+            flags.paragon ?? 0
+        }" data-dtype="Number"/>
+            <span class="sep"> / </span>
+            <input type="text" name="flags.me5e.renegade" placeholder="0" value="${
+                flags.renegade ?? 0
+            }" data-dtype="Number"/>
+            
+        </div>
+
+
+        <footer class="attribute-footer">
+            <span class="spell-dc" >Paragon</span>
+            <span class="sep"> / </span>
+            <span class="spell-dc" >Renegade</span>
+        </footer>
+            
+        
+    </li>
+	
+	`);
+});
---
+++ src/me5eTEST.js
@@ -0,0 +1,416 @@
+
+Hooks.once("init", () => {
+
+    CONFIG.DND5E.currencies = {
+    	  gp: { label: "Credits", abbreviation: "cr" }
+    	};
+      
+	
+
+//Create new weapon properties
+    CONFIG.DND5E.itemProperties.arc = {
+        label: "Arc",
+        isPhysical: true,
+        reference: "Compendium.mass-effect-5e.me5e-r-rules.JournalEntry.coUqupVCukVK3vFc.JournalEntryPage.viRtMNVmzOHD5pTX"
+    };
+    CONFIG.DND5E.itemProperties.bfr = {
+        label: "Burst Fire",
+        isPhysical: true
+    };
+    CONFIG.DND5E.itemProperties.dtp = {
+        label: "Double Tap",
+        isPhysical: true
+    };
+    CONFIG.DND5E.itemProperties.heat = {
+        label: "Heat",
+        isPhysical: true
+    };
+    CONFIG.DND5E.itemProperties.hip = {
+        label: "Hip Fire",
+        isPhysical: true
+    };
+    CONFIG.DND5E.itemProperties.mel = {
+        label: "Melee",
+        isPhysical: true
+    };
+    CONFIG.DND5E.itemProperties.rcl = {
+        label: "Recoil",
+        isPhysical: true
+    };
+    CONFIG.DND5E.itemProperties.slt = {
+        label: "Silent",
+        isPhysical: true
+    };
+    CONFIG.DND5E.itemProperties.vtd = {
+        label: "Vented",
+        isPhysical: true
+    };
+
+    //Add new weapon properties to "valid"
+    CONFIG.DND5E.validProperties.weapon.add("arc");
+    CONFIG.DND5E.validProperties.weapon.add("bfr");
+    CONFIG.DND5E.validProperties.weapon.add("dtp");
+    CONFIG.DND5E.validProperties.weapon.add("heat");
+    CONFIG.DND5E.validProperties.weapon.add("hip");
+    CONFIG.DND5E.validProperties.weapon.add("mel");
+    CONFIG.DND5E.validProperties.weapon.add("rcl");
+    CONFIG.DND5E.validProperties.weapon.add("slt");
+    CONFIG.DND5E.validProperties.weapon.add("vtd");
+
+    //Remove depricated weapon properties from "valid"
+    CONFIG.DND5E.validProperties.weapon.delete("ada");
+    CONFIG.DND5E.validProperties.weapon.delete("amm");
+    CONFIG.DND5E.validProperties.weapon.delete("fir");
+    CONFIG.DND5E.validProperties.weapon.delete("foc");
+    CONFIG.DND5E.validProperties.weapon.delete("lod");
+    CONFIG.DND5E.validProperties.weapon.delete("mgc");
+    CONFIG.DND5E.validProperties.weapon.delete("rel");
+    CONFIG.DND5E.validProperties.weapon.delete("ret");
+    CONFIG.DND5E.validProperties.weapon.delete("sil");
+
+
+//Create new skills
+    CONFIG.DND5E.skills.ele = {
+        label: "Electronics",
+        ability: "int",
+        fullkey: "electronics",
+        reference: "Compendium.mass-effect-5e.me5e-r-rules.JournalEntry.R0dKAZvHn6z3oylI.JournalEntryPage.xUwZdpm6F1PUa2mK",
+        icon: "" //#TODO
+    };
+    CONFIG.DND5E.skills.eng = {
+        label: "Engineering",
+        ability: "int",
+        fullkey: "engineering",
+        reference: "Compendium.mass-effect-5e.me5e-r-rules.JournalEntry.R0dKAZvHn6z3oylI.JournalEntryPage.qXY39DJTcAbz0r6B",
+        icon: "" //#TODO
+    };
+    CONFIG.DND5E.skills.sci = {
+        label: "Science",
+        ability: "int",
+        fullkey: "science",
+        reference: "Compendium.mass-effect-5e.me5e-r-rules.JournalEntry.R0dKAZvHn6z3oylI.JournalEntryPage.pmQWAbKyuF5kkgNf",
+        icon: "" //#TODO
+    };
+    CONFIG.DND5E.skills.veh = {
+        label: "Vehicle Handling",
+        ability: "dex",
+        fullkey: "vehiclehandling",
+        reference: "Compendium.mass-effect-5e.me5e-r-rules.JournalEntry.R0dKAZvHn6z3oylI.JournalEntryPage.xVjyRLNRenSuPieh",
+        icon: "" //#TODO
+    };
+
+    //Remove depricated skill
+    delete CONFIG.DND5E.skills.ani;
+    delete CONFIG.DND5E.skills.arc;
+    delete CONFIG.DND5E.skills.nat;
+    delete CONFIG.DND5E.skills.rel;
+
+
+//Create new equipment types
+    CONFIG.DND5E.miscEquipmentTypes.program = "Program";
+    CONFIG.DND5E.miscEquipmentTypes.armMod = "Armor Mod";
+    CONFIG.DND5E.miscEquipmentTypes.wepMod = "Weapon Mod";
+    CONFIG.DND5E.miscEquipmentTypes.bodArm = "Body Armor";
+
+    //Remove depricated equipment types
+    delete CONFIG.DND5E.miscEquipmentTypes.wand;
+    delete CONFIG.DND5E.miscEquipmentTypes.rod;
+    delete CONFIG.DND5E.miscEquipmentTypes.wondrous;
+
+
+//Create new consumable types
+    CONFIG.DND5E.consumableTypes.singProg = "Single-Use Program";
+    CONFIG.DND5E.consumableTypes.grenade = "Grenade";
+    CONFIG.DND5E.consumableTypes.narcotic = "Narcotic";
+
+    //Remove depricated consumable types
+    delete CONFIG.DND5E.consumableTypes.scroll;
+    delete CONFIG.DND5E.consumableTypes.wand;
+    delete CONFIG.DND5E.consumableTypes.rod; 
+    delete CONFIG.DND5E.consumableTypes.potion;
+
+    
+//Create new weapon types
+    CONFIG.DND5E.weaponTypes.aRifle = "Assault Rifle";
+    CONFIG.DND5E.weaponTypes.hPistol = "Heavy Pistol";
+    CONFIG.DND5E.weaponTypes.smg = "Sub-machine Gun";
+    CONFIG.DND5E.weaponTypes.sGun = "Shotgun";
+    CONFIG.DND5E.weaponTypes.sRifle = "Sniper Rifle";
+    CONFIG.DND5E.weaponTypes.melee = "Melee";
+    CONFIG.DND5E.weaponTypes.heavy = "Heavy";
+
+    CONFIG.DND5E.weaponIds.assaultRifle = "Compendium.mass-effect-5e.item-weapons-me5e-r.Item.mdMOsXHsH1ExSyk7";
+    CONFIG.DND5E.weaponIds.heavyPistol = "Compendium.mass-effect-5e.item-weapons-me5e-r.Item.rCSJsC4y6JT1zSQv";
+    CONFIG.DND5E.weaponIds.subMachineGun = "Compendium.mass-effect-5e.item-weapons-me5e-r.Item.yFN7A6oYEAA10YwH";
+    CONFIG.DND5E.weaponIds.shotGun = "Compendium.mass-effect-5e.item-weapons-me5e-r.Item.Nn2dPAJon6cJUaLe";
+    CONFIG.DND5E.weaponIds.sniperRifle = "Compendium.mass-effect-5e.item-weapons-me5e-r.Item.XGCY9ZqdPuKGdzpV";
+    CONFIG.DND5E.weaponIds.melee = "Compendium.mass-effect-5e.item-weapons-me5e-r.Item.mAtAsxTcfYYkKTIZ";
+
+    //Remove depricated weapon types
+    delete CONFIG.DND5E.weaponTypes.simpleM;
+    delete CONFIG.DND5E.weaponTypes.simpleR;
+    delete CONFIG.DND5E.weaponTypes.martialM;
+    delete CONFIG.DND5E.weaponTypes.martialR;
+    delete CONFIG.DND5E.weaponTypes.natural;
+    delete CONFIG.DND5E.weaponTypes.improv;
+    delete CONFIG.DND5E.weaponTypes.siege;
+
+    delete CONFIG.DND5E.weaponProficiencies.sim;
+    delete CONFIG.DND5E.weaponProficiencies.mar;
+
+    delete CONFIG.DND5E.weaponIds.battleaxe;
+    delete CONFIG.DND5E.weaponIds.blowgun;
+    delete CONFIG.DND5E.weaponIds.club;
+    delete CONFIG.DND5E.weaponIds.dagger;
+    delete CONFIG.DND5E.weaponIds.dart;
+    delete CONFIG.DND5E.weaponIds.flail;
+    delete CONFIG.DND5E.weaponIds.glaive;
+    delete CONFIG.DND5E.weaponIds.greataxe;
+    delete CONFIG.DND5E.weaponIds.greatclub;
+    delete CONFIG.DND5E.weaponIds.greatsword;
+    delete CONFIG.DND5E.weaponIds.halberd;
+    delete CONFIG.DND5E.weaponIds.handaxe;
+    delete CONFIG.DND5E.weaponIds.handcrossbow;
+    delete CONFIG.DND5E.weaponIds.heavycrossbow;
+    delete CONFIG.DND5E.weaponIds.javelin;
+    delete CONFIG.DND5E.weaponIds.lance;
+    delete CONFIG.DND5E.weaponIds.lightcrossbow;
+    delete CONFIG.DND5E.weaponIds.lighthammer;
+    delete CONFIG.DND5E.weaponIds.longbow;
+    delete CONFIG.DND5E.weaponIds.longsword;
+    delete CONFIG.DND5E.weaponIds.mace;
+    delete CONFIG.DND5E.weaponIds.maul;
+    delete CONFIG.DND5E.weaponIds.morningstar;
+    delete CONFIG.DND5E.weaponIds.net;
+    delete CONFIG.DND5E.weaponIds.pike;
+    delete CONFIG.DND5E.weaponIds.quarterstaff;
+    delete CONFIG.DND5E.weaponIds.rapier;
+    delete CONFIG.DND5E.weaponIds.scimitar;
+    delete CONFIG.DND5E.weaponIds.shortsword;
+    delete CONFIG.DND5E.weaponIds.sickle;
+    delete CONFIG.DND5E.weaponIds.spear;
+    delete CONFIG.DND5E.weaponIds.shortbow;
+    delete CONFIG.DND5E.weaponIds.sling;
+    delete CONFIG.DND5E.weaponIds.trident;
+    delete CONFIG.DND5E.weaponIds.warpick;
+    delete CONFIG.DND5E.weaponIds.warhammer;
+    delete CONFIG.DND5E.weaponIds.whip;
+
+    
+//Add Item Rarity
+    CONFIG.DND5E.itemRarity.spectre = 'Spectre'
+    delete CONFIG.DND5E.itemRarity.legendary;
+    delete CONFIG.DND5E.itemRarity.artifact;
+
+/*
+//Create new "schools of magic"
+    CONFIG.DND5E.spellSchools.biotics = {
+        label: "Biotics",
+        icon: "",
+        fullKey: "biotics",
+        reference: ""
+    };
+    CONFIG.DND5E.spellSchools.tech = {
+        label: "Tech",
+        icon: "",
+        fullKey: "tech",
+        reference: ""
+    };
+    CONFIG.DND5E.spellSchools.combatpowers = {
+        label: "Combat Powers",
+        icon: "",
+        fullKey: "combatpowers",
+        reference: ""
+    };
+*/
+
+    CONFIG.DND5E.spellSchools.con.label = "Tech";
+
+
+//Create tools
+    CONFIG.DND5E.toolTypes.starship = "Starship";
+
+    CONFIG.DND5E.tools.armorSmith = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.item-gear-me5e-r.Item.0fakcHNiJ3JxYGB4"
+    };
+
+    CONFIG.DND5E.tools.chemist = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.item-gear-me5e-r.Item.HoZ3Ieg0SsNpaVJO"
+    };
+
+    CONFIG.DND5E.tools.hacking = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.item-gear-me5e-r.Item.Lu8joXlut3wrp0jm"
+    };
+
+    CONFIG.DND5E.tools.medical = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.item-gear-me5e-r.Item.WjxpzqH7SSC33uS4"
+    };
+
+    CONFIG.DND5E.tools.musical = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.item-gear-me5e-r.Item.wupPhivdYQifa0cL"
+    };
+
+    CONFIG.DND5E.tools.tailor = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.item-gear-me5e-r.Item.9KxP2pyyNxOpHDJE"
+    };
+
+    CONFIG.DND5E.tools.weaponSmith = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.item-gear-me5e-r.Item.tUHnf4csZ5NLvcmJ"
+    };
+
+    CONFIG.DND5E.tools.starDrive = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.vehicle-system-actions-me5e-r.Item.XUcyHg59IWKfgbHD"
+    };
+
+    CONFIG.DND5E.tools.starEWS = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.vehicle-system-actions-me5e-r.Item.AuVEMnBZo9zvqASa"
+    };
+
+    CONFIG.DND5E.tools.starHelm = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.vehicle-system-actions-me5e-r.Item.Yc3f7wDEmPLQ6hfH"
+    };
+
+    CONFIG.DND5E.tools.starNav = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.vehicle-system-actions-me5e-r.Item.NTxzvwfl58SOmHoB"
+    };
+
+    CONFIG.DND5E.tools.starSSC = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.vehicle-system-actions-me5e-r.Item.jxXkdRqkQWLefnMf"
+    };
+
+    CONFIG.DND5E.tools.starWeapons = {
+        ability: "int",
+        id: "Compendium.mass-effect-5e.vehicle-system-actions-me5e-r.Item.oxqbqZMgsPkyhNW6"
+    };
+
+    delete CONFIG.DND5E.tools.alchemist;
+    delete CONFIG.DND5E.tools.bagpipes;
+    delete CONFIG.DND5E.tools.calligrapher;
+    delete CONFIG.DND5E.tools.carpenter;
+    delete CONFIG.DND5E.tools.cartographer;
+    delete CONFIG.DND5E.tools.cobbler;
+    delete CONFIG.DND5E.tools.drum;
+    delete CONFIG.DND5E.tools.dulcimer;
+    delete CONFIG.DND5E.tools.flute;
+    delete CONFIG.DND5E.tools.forg;
+    delete CONFIG.DND5E.tools.glassblower;
+    delete CONFIG.DND5E.tools.herb;
+    delete CONFIG.DND5E.tools.horn;
+    delete CONFIG.DND5E.tools.jeweler;
+    delete CONFIG.DND5E.tools.leatherworker;
+    delete CONFIG.DND5E.tools.lute;
+    delete CONFIG.DND5E.tools.lyre;
+    delete CONFIG.DND5E.tools.mason;
+    delete CONFIG.DND5E.tools.navg;
+    delete CONFIG.DND5E.tools.panflute;
+    delete CONFIG.DND5E.tools.potter;
+    delete CONFIG.DND5E.tools.shawm;
+    delete CONFIG.DND5E.tools.smith;
+    delete CONFIG.DND5E.tools.viol;
+    delete CONFIG.DND5E.tools.weaver;
+    delete CONFIG.DND5E.tools.woodcarver;
+
+    delete CONFIG.DND5E.toolTypes.music;
+
+
+//Add senses
+    CONFIG.DND5E.senses.tremorsense = 'Infrared';
+
+});
+
+
+const prep = dnd5e.documents.Actor5e.prototype.prepareBaseData
+function extendActorData() {
+	const health = this.system.attributes.hp;
+	
+	if (this.type === "npc" || this.type === "character") {
+		health["shields"] = health["shields"] || 0;
+		health["shieldsMax"] = health["shieldsMax"] || 0;
+		health["shieldsRegen"] = health["shieldsRegen"] || 0;
+	}
+
+
+    return prep.call(this);
+}
+
+/*
+Hooks.on('renderActorSheet', (app, html, data) => {
+    const healthdiv = html
+        .find('div.meter-group').first();
+    const flags = data.actor.flags.me5e || {};
+    const pct = flags.shields / flags.shieldsMax * 100;
+    healthdiv.before(`
+                        <div class="meter-group">
+                            <div class="label roboto-condensed-upper">
+                                <span>Shields</span>
+                                <a class="config-button" data-action="hitPoints" data-tooltip="Configure Shields"
+                                   aria-label="Configure Shields">
+                                    <i class="fas fa-cog"></i>
+                                </a>
+                            </div>
+                            <div class="meter sectioned hit-points">
+                                <div class="progress hit-points"
+                                     role="meter" aria-valuemin="0" aria-valuenow="${flags.shields ?? 0}"
+                                     aria-valuemax="${flags.shieldsMax ?? 0}" style="--bar-percentage: ${pct ?? 0}%;">
+                                    <div class="label">
+                                        <span class="value">${flags.shields ?? 0}</span>
+                                        <span class="separator">&sol;</span>
+                                        <span class="max">${flags.shieldsMax ?? 0}</span>
+                                    </div>
+                                    <input type="text" name="flags.me5e.shields" data-dtype="Number"
+                                           placeholder="0" value="${flags.shields ?? 0}" hidden>
+                                </div>
+                            </div>
+                        </div>
+    `);
+
+})
+*/
+/*
+Hooks.on('renderActorSheet', (app, html, data) => {
+    const healthdiv = html
+        .find('header.sheet-header')
+        .find('ul.attributes.flexrow');
+    const flags = data.actor.flags.me5e || {};
+    healthdiv.prepend(`
+		  		<li class="attribute shields">
+                    <h4 class="attribute-name box-title">Shields</h4>
+                    <div class="attribute-value multiple">
+                        <input name="flags.me5e.shields" type="text" value="${flags.shields ?? 0}" data-dtype="Number" placeholder="5"/>
+                        <span class="sep"> / </span>
+                        <input name="flags.me5e.shieldsMax" type="text" value="${flags.shieldsMax ?? 0}" data-dtype="Number" placeholder="5"/>
+                    </div>
+                    <footer class="attribute-footer">
+                        <input name="flags.me5e.shieldsRegen" type="text" class="shieldsRegen" placeholder="Shield Regen." value="${flags.shieldsRegen ?? 0}" data-dtype="Number"/>
+                    </footer>
+                </li>
+	  `);
+
+
+    const counters = html.find('div.counters');
+    counters.append(`
+	<div class="counter flexrow indoctrination">
+    <h4> Indoctrination </h4>
+    <div class="counter-value">
+      <input type="text" name="flags.me5e.indoctrination" placeholder="0" value="${
+          flags.indoctrination ?? 0
+      }" data-dtype="Number"/>
+    </div>
+	</div>
+	
+	`);
+});
+
+*/
